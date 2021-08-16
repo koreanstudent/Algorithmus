0c7feb66ab3d7a82fc887db4cf23fce67a290c42
이창현 <hn12344@naver.com>
2021-08-16 22:19:19 +0900

    4. 피보나치 수열

--- src/main/java/inflearn/RecursiveAndTreeAndGraph/java04.java
+++ src/main/java/inflearn/RecursiveAndTreeAndGraph/java04.java
@@ -17,7 +17,7 @@ package inflearn.RecursiveAndTreeAndGraph;
 public class java04 {
     static int[] fibo;
     public int DFS(int n) {
-
+        if(fibo[n]>0) return fibo[n];
         if(n==1) return fibo[n] = 1;
         else if(n==2) return fibo[n] = 1;
         else return fibo[n] = DFS(n-2) + DFS(n-1);
